3d08c69dd3ef1ab8d2c54dd7bc5675ae11446b90
mikiqex <michal.novak@qex.cz>
2016-02-23 12:26:17 +0100

    Update qetrix.php

--- qetrix.php
+++ qetrix.php
@@ -1,23 +1,25 @@
-<?php namespace com\qetrix;
+<?php
+namespace com\qetrix;
 
-/* Copyright (c) 2015 QetriX. Licensed under MIT License, see /LICENSE.txt file.
- * Initial/Startup/Index file, called by e.g. .htaccess on Apache HTTP Server with mod_rewrite
+/* Copyright (c) QetriX.com. Licensed under MIT License, see /LICENSE.txt file.
+ * 16.01.04 | Initial/Startup/Index file, called by webserver rewrite engine
  */
 
 //ini_set("display_errors", 1);ini_set("display_startup_errors", 1);error_reporting(-1);
 
+use com\qetrix\libs\QApp;
 use com\qetrix\libs\Util;
 
 require __DIR__."/libs/qapp.php";
 
 // Create QetriX App Instance
-$app = libs\QApp::getInstance();
+$app = QApp::getInstance();
 
 // Read path from GET variable
 $path = $app->envDS()->get("get", "_p");
 
 // Parse path
-$modVars = $app->getModule($path);
+$modVars = $app->parsePath($path);
 
 // Call QetriX App Module
 $modOutput = $app->loadModule($modVars); // Decode path in GET variable "_p", load module and call func. Then return output
