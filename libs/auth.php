5809f4ed33994c49a686203ab3e94ccc35abce66
mikiqex <michal.novak@qex.cz>
2016-06-01 14:11:34 +0200

    Update auth.php

--- libs/auth.php
+++ libs/auth.php
@@ -1,48 +1,64 @@
-<?php
+<?php declare(strict_types = 1);
 namespace com\qetrix\libs;
 
 /* Copyright (c) QetriX.com. Licensed under MIT License, see /LICENSE.txt file.
- * 16.03.04 | Authentication for users and apps
+ * 16.04.18 | Authentication for users and apps
  */
 
-/* TODO: It's too dependent with HTTP DS. Token can be pased as get(_t) and returned as header(token)
-*/
+use com\qetrix\apps\common\datastores\MySQL;
+
+// TODO: It's too dependent with HTTP DS. Token can be pased as get(_t) and returned as header(token)
 
 class Auth
 {
 	private $_token = null;
 	private $_isAuth = false;
-	private $userInfo = null;
+	private $userInfo = [];
+	private $groups = [];
 
 	private $loginFormUsername = "loginform-username";
 	private $loginFormPassword = "loginform-password";
+	private $loginFormGoTo = "loginform-goto";
 
+	/** @var MySQL */
 	private $ds;
-	/** @var QApp */
-	private $app;
+	/** @var QPage */
+	private $_page;
 
 	function __construct(DataStore $ds = null)
 	{
 		return $this->Auth($ds);
 	}
 
+	// TODO: Add REMOTE_USER support
 	function Auth(DataStore $ds = null)
 	{
 		if ($this->isAuth()) return $this;
-		$this->app = QApp::getInstance();
-		$this->ds = $ds === null ? $this->app->ds() : $ds;
-		$oldToken = $this->app->envDS()->get("cookie", "token") ?: $this->app->envDS()->get("get", "_t");
+		$this->_page = QPage::getInstance();
+		if ($this->page()->get("remote_user") != "") {
+		} elseif ($this->page()->get("auth_user") != "") {
+
+		} else {
+			$this->ds = $ds === null ? $this->page()->ds() : $ds;
+		}
+		$oldToken = $this->page()->get($this->tokenName());
 		if ($oldToken != "") {
 			$newToken = $this->token();
-			if ($this->ds->authToken($oldToken, $newToken, Util::crc32($this->app->envDS()->get("env", "user_agent")))) $this->token($newToken); else $this->logout();
+			if ($this->ds->authToken($oldToken, $newToken, Util::crc32($this->page()->get("user_agent")))) $this->token($newToken); else $this->logout();
 		}
 		return $this;
 	}
 
-	function setFormFields($username, $password)
+	function page()
+	{
+		return $this->_page;
+	}
+
+	function setFormFields($username, $password, $goto = null)
 	{
 		$this->loginFormUsername = $username;
 		$this->loginFormPassword = $password;
+		if ($goto !== null) $this->loginFormGoTo = $goto;
 	}
 
 	function signup($username, $password, $password2, $email)
@@ -52,20 +68,23 @@ class Auth
 		return $id;
 	}
 
-	function login($data)//$username = null, $password = null)
+	function login($data) //$username = null, $password = null)
 	{
 		if (!isset($data[$this->loginFormUsername]) || !isset($data[$this->loginFormPassword])) return false;
 		if ($this->isAuth()) return true;
-
-		$ua = $this->app->envDS()->get("env", "user_agent");
-		$ip = $this->app->envDS()->get("env", "remote_addr");
+		$ua = $this->page()->get("user_agent");
+		$ip = $this->page()->get("remote_addr");
 		$newToken = $this->token();
 		$userdata = $this->ds->authLogin($data[$this->loginFormUsername]);
-		//Util::log($userdata, "auth/login/userdata");
+		//Util::log($userdata, "auth.login.userdata");
 		if ($userdata !== null) {
 			if (password_verify($data[$this->loginFormPassword], $userdata["password"])) {
 				$this->ds->authLoginSet($userdata["id"], $newToken, Util::crc32($ua), $ip, $this->getOS($ua).$this->getBrowser($ua));
 				$this->token($newToken);
+				if (isset($data[$this->loginFormGoTo]) && strpos($data[$this->loginFormGoTo], "//") === false) {
+					$this->page()->set("location", $this->loginFormGoTo);
+					exit();
+				}
 			} else return false;
 		}
 		return $this->_isAuth;
@@ -74,17 +93,31 @@ class Auth
 	function get($key)
 	{
 		if ($this->ds === null) return "";
-		if ($this->userInfo == null) {
-			$info = $this->ds->authDetail($this->_token);
-			$this->userInfo = count($info) > 0 ? $info[0] : null;
+		if ($this->userInfo == []) {
+			//$info = $this->ds->authDetail($this->_token);
+			//$this->userInfo = count($info) > 0 ? $info[0] : null;
+			$this->userInfo = $this->ds->authDetail($this->_token); // *MNo 20160502
 		}
 		if (!isset($this->userInfo[$key])) return "";
 		return $this->userInfo[$key];
 	}
 
+	function getGroups()
+	{
+		if ($this->groups == []) $this->groups = $this->ds->authGroups($this->get("id"));
+		return $this->groups;
+	}
+
+	function memberOf($group)
+	{
+		if ($this->groups == []) $this->getGroups();
+		return (isset($this->groups[$group]));
+	}
+
 	function checkAccess($module, $method, $pid = null)
 	{
-		$this->ds->authAccess($module, $method, $pid, $this->get("id"));
+		if ($this->groups == []) $this->getGroups();
+		$this->ds->authAccess($module, $method, $this->groups, $pid);
 	}
 
 	function logout()
@@ -92,7 +125,7 @@ class Auth
 		$this->_isAuth = false;
 		$this->_token = null;
 		$this->setToken("");
-		$ua = $this->app->envDS()->get("env", "user_agent");
+		$ua = $this->page()->get("user_agent");
 		$this->ds->authClose($this->_token, Util::crc32($ua));
 	}
 
@@ -107,8 +140,13 @@ class Auth
 
 	private function setToken($value)
 	{
-		$this->app->envDS()->set("cookie", "token", $value);
-		$this->app->envDS()->set("header", "token", $value);
+		$this->page()->set($this->tokenName(), $value);
+		//$this->app()->envDS()->set("header", $this->tokenName, $value); // TODO
+	}
+
+	private function tokenName()
+	{
+		return $this->page()->authTokenName();
 	}
 
 	/** Generate new password */
@@ -176,4 +214,15 @@ class Auth
 		foreach ($br as $regex => $value) if (preg_match($regex, $userAgent)) return $value;
 		return "XX";
 	}
+}
+
+final class UserState
+{
+	const blocked = -1;
+	const created = 0;
+	const valid = 1;
+	const renewpw = 2;
+	const lostpw = 3;
+	const admin = 8;
+	const group = 9;
 }
